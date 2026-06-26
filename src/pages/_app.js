f8f2fd1fb3e1bf2655ae7f9bdeb14e9dd5bb5515
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-06-26 17:04:30 -0300

    ok

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,7 @@
-// pages/_app.js
-import { DiscordProvider } from '@/contexts/DiscordContext';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <DiscordProvider>
-      <Component {...pageProps} />
-    </DiscordProvider>
+    <Component {...pageProps} />
   );
 }
